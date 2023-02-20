1de3dccbba80b462f8c469235686fa1cc94d77a5
Marksu-u <72389013+Marksu-u@users.noreply.github.com>
2023-02-20 16:54:10 +0100

    started login & register

---
+++ src/pages/register/register.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Header from "@/components/Header/Header";
+import Footer from "@/components/Footer/Footer";
+
+const Register = () => {
+    return (
+        <div>
+        <Header />
+        <h1>Register</h1>
+        <Footer />
+        </div>
+    );
+    };
+
+export default Register;
