057b369d7c80c36b59d77144a2f3bcc931b9de30
Marksu-u <72389013+Marksu-u@users.noreply.github.com>
2023-02-23 10:14:58 +0100

    multicards component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,13 @@
 import React from "react"
 import Header from '@/components/Header/Header';
 import Footer from '@/components/Footer/Footer';
+import Card from "@/components/Card/Card";
 
 
 const HomePage = () => {
   return (
     <div>
+      <Card />
     </div>
   )
 }
