ec0f8a0af32eaa4fd713b2e3d4c5ba83b9a46e79
Marksu-u <Marksu-u@users.noreply.github.com>
2023-03-02 09:04:26 +0100

    api

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,10 +1,10 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:4000/api/v1';
+const API_URL = 'https://localhost:5000/api/v1/'
 
 export default {
   register(body) {
-    return axios.post('http://localhost:4000//api/v1/register', body, {
+    return axios.post('http://localhost:5000/api/v1/register', body, {
       headers: {
         'Content-type': 'application/json'
       }
